0b88cba36bcba42ae915d702374546edbaf93c45
Steven Exil <117109826+stexil@users.noreply.github.com>
2024-05-18 15:00:45 -0400

    Add files via upload

---
+++ departureFilter.py
@@ -0,0 +1,79 @@
+import time 
+import datetime
+from datetime import date
+#Gets number of flights given a csv file to read, a start time [incluive], an end time [inclusive], and the date.
+#date MUST be in 'yyyy-mm-dd' format, if you want to read data from today, leave date empty!
+#insert time values as normal in string format.
+#EXAMPLE: getNumberOf('data.csv', '11:00', '11:60', '2024-05-10')
+def getNumberOf(fname, start, end, dateInput = str(date.today())):
+    
+    sameDateList = []
+    inRangeList = []
+
+    if fname == None:
+        print("File name cannot be null")
+        return 0
+    file = open(fname, "r")
+    header  = file.readline()
+    dataList = file.readlines()
+    file.close()
+    for element in dataList:
+        elementList = element.strip().split(",")
+        #invokes date filter
+        if elementList[0] == dateInput:
+            sameDateList.append(elementList)
+        #start/end filter
+    for elementList in sameDateList:
+        time = int(elementList[1].replace(":", ""))
+        startInt = int(start.replace(":", ""))
+        endInt = int(end.replace(":", ""))
+        if time >= startInt and time <= endInt:
+            inRangeList.append(elementList)     
+    if len(sameDateList) == 0:
+        print("No data present for given date! **check format in docs**")
+        return 0
+    if len(inRangeList) == 0:
+        print("No such data in given range")
+        return 0
+    return (len(inRangeList))
+    
+#HELPER METHOD.. IGNORE
+def addMinutesToTime(military_time, minutes_to_add):
+    hours, minutes = map(int, military_time.split(':'))
+    total_minutes = hours * 60 + minutes
+    total_minutes += int(minutes_to_add)  # Ensure minutes_to_add is an integer
+    total_minutes %= 24 * 60
+    new_hours = total_minutes // 60
+    new_minutes = total_minutes % 60
+    result = '{:02d}:{:02d}'.format(new_hours, new_minutes)
+    return result
+
+#Uses getNumberOf() to invoke the number of flights, where fromNow is minutes (integer)
+# NOTE: can also work if the time  DOES NOT falls over to the next day. 
+# EXAMPLE: getNumberOfFromNow('data.csv' fromNow)--> returns 
+def getNumberOfFromNow(fname, fromNow):
+    if fname == None or fname == "":
+        print("Error: File name can not be null")
+    currentTimeStr = datetime.datetime.now()
+    currentTimeStr = str(currentTimeStr)[11:16]
+    endtime = addMinutesToTime(currentTimeStr, fromNow)
+
+    
+    if int(endtime.replace(":", "")) <= 2359 and int(currentTimeStr.replace(":", "") <= endtime):
+        return getNumberOf(fname, currentTimeStr, endtime)
+    #CHECK NOTE
+    else:
+        print("Multi-day collection is not allowed for methods using currentDay!")
+        return 0
+
+    
+
+    
+    
+
+
+
+#print(getNumberOf('data.csv', '0:00', '23:59', '2024-05-10'))
+#print(getNumberOf('data.csv', '18:49', '18:59', '2024-05-12'))
+print(getNumberOfFromNow("Data.csv", 100))
+
---
+++ flightStatScrapper.py
@@ -0,0 +1,140 @@
+#https://www.flightstats.com/v2/flight-tracker/departures/ATL
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import array as arr
+import time 
+from datetime import date
+import schedule
+
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.flightstats.com/v2/flight-tracker/departures/ATL")
+departureData = []
+
+def cookieAccepter():
+    accept = driver.find_element(by=By.ID, value="onetrust-close-btn-container")
+    accept.click()
+def tidyData(lineData):
+        splitLinedata = lineData.split(" ")
+        flightInfo = splitLinedata[0] + " " + splitLinedata[1]
+        departureTime = splitLinedata[2]
+        arrivalTime = splitLinedata[3]
+        Destination = splitLinedata[-1]
+        stringBuilder = ""
+        if ("/" not in splitLinedata):
+            for i in range(5, len(splitLinedata) - 1):
+                stringBuilder += splitLinedata[i] + " "
+            stringBuilder.strip()
+            airlineInfo = stringBuilder.replace(",", "/")  
+        else:
+            slashIndex = splitLinedata.index("/")
+            array1 = splitLinedata[5:slashIndex]
+            for element in array1:
+                stringBuilder += element + " "
+            stringBuilder.strip() 
+            airlineInfo = stringBuilder.replace(",", "/")  
+        finalArray = [departureTime, arrivalTime, flightInfo, Destination, airlineInfo]
+        return finalArray
+
+def getSubPages(): 
+    paginationBar = driver.find_element(by=By.XPATH, value = "/html/body/div[1]/div/section/div/div[2]/div[3]/div")
+    value = ""
+    iterator = 3
+    count = -1
+    while(value != ("→")):
+        currentElement = driver.find_element(by=By.XPATH, value = "/html/body/div[1]/div/section/div/div[2]/div[3]/div/div[{}]".format(iterator))
+        value = currentElement.text
+        count += 1
+        iterator += 1
+    return count
+
+def time0to6():
+    timePeriodButton = driver.find_element(by=By.NAME, value="time")
+    selecter = Select(timePeriodButton)
+    selecter.select_by_value("0")
+    filterbutton = driver.find_element(By.CLASS_NAME, "basic-button__Button-sc-3qdr1i-0.kmYwtt")
+    filterbutton.click()
+    time.sleep(1)
+
+
+def time6to12():
+    timePeriodButton = driver.find_element(by=By.NAME, value="time")
+    selecter = Select(timePeriodButton)
+    selecter.select_by_value("6")
+    filterbutton = driver.find_element(By.CLASS_NAME, "basic-button__Button-sc-3qdr1i-0.kmYwtt")
+    filterbutton.click()
+    time.sleep(1)
+
+
+def time12to18():
+    timePeriodButton = driver.find_element(by=By.NAME, value="time")
+    selecter = Select(timePeriodButton)
+    selecter.select_by_value("12")
+    filterbutton = driver.find_element(By.CLASS_NAME, "basic-button__Button-sc-3qdr1i-0.kmYwtt")
+    filterbutton.click()
+    time.sleep(1)
+
+def time18to24():
+    timePeriodButton = driver.find_element(by=By.NAME, value="time")
+    selecter = Select(timePeriodButton)
+    selecter.select_by_value("18")
+    filterbutton = driver.find_element(By.CLASS_NAME, "basic-button__Button-sc-3qdr1i-0.kmYwtt")
+    filterbutton.click()
+    time.sleep(1)
+
+def mainScraper():
+    pagesToGoThrough = getSubPages()
+    for x in range(0, pagesToGoThrough):
+        table = driver.find_element(by=By.XPATH, value = "/html/body/div[1]/div/section/div/div[2]/div[2]")
+        arrayData = table.text.split("\n")
+        lengthOfRows = (int)(len(arrayData[4:]) / 6) + 2
+        #DIVIDE ARRAY LENGTH BY 6 TO GET EACH INDIVIDUAL LENGTH
+        for i in range(2, int(lengthOfRows)):
+            #rowElement = driver.find_element(by=By.XPATH, value = "/html/body/div[1]/div/section/div/div[2]/div[2]/a[{}]".format(i))
+            rowElement = driver.find_element(by=By.XPATH, value="//div[@id='__next']/div/section/div/div[2]/div[2]/a[{}]".format(i))
+            stringLine = rowElement.text.replace("\n", " ")
+            newrow = tidyData(stringLine)
+            if newrow != None and len(newrow) == 5:
+                departureData.append(newrow)
+        nextButton = driver.find_element(by=By.XPATH, value = "/html/body/div[1]/div/section/div/div[2]/div[3]/div/div[{}]".format(pagesToGoThrough + 3))
+        nextButton.click()
+   
+def createNewCSV(fname):
+    file = open(fname, "w")
+    file.write("Date, Departure Time, Arrival Time, Flight Info, Destination, Airline Info\n")
+    file.close()
+
+def addDataToCSV(fname, dataToAdd):
+    dataToAdd.sort()
+    file = open(fname, "a")
+    for element in dataToAdd:
+        line = '{},{},{},{},{},{}\n'.format(str(date.today()), str(element[0]), str(element[1]), str(element[2]), str(element[3]), str(element[4]))
+        file.write(line)
+    file.close()
+
+    
+
+cookieAccepter()
+time0to6()
+mainScraper()
+time6to12()
+mainScraper()
+time12to18()
+mainScraper()
+time18to24()
+mainScraper()
+
+#createNewCSV("data.csv")
+addDataToCSV("data.csv", departureData)
+
+time.sleep(2)
+driver.quit()
+
+    
+
